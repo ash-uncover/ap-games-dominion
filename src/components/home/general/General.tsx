82f568c9e3edac6009090bf29a8cb90e87cab9e4
ash-uncover <ash.uncover@gmail.com>
2024-11-22 10:42:38 +0100

    add some translations

--- src/components/home/general/General.tsx
+++ src/components/home/general/General.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
-import { Outlet, useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
+// Components
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Menu, PanelButton } from '@uncover/games-common'
 // CSS
 import './General.css'
 
@@ -9,6 +13,8 @@ export const General = ({
 }: GeneralProperties) => {
 
   // #region Hooks
+  const { t } = useTranslation()
+  const location = useLocation()
   const navigate = useNavigate()
   // #endregion
 
@@ -19,6 +25,36 @@ export const General = ({
   // #endregion
 
   // #region Rendering
+  const items = [{
+    icon: <FontAwesomeIcon icon={['fas', 'gamepad']} />,
+    selected: location.pathname === '/games',
+    text: t('home.local.menu'),
+    onClick: () => { navigate('/games') }
+  }, {
+    icon: <FontAwesomeIcon icon={['fas', 'gamepad']} />,
+    disabled: true,
+    selected: location.pathname === '/online',
+    text: t('home.online.menu'),
+    onClick: () => { navigate('/online') }
+  }, {
+    icon: <FontAwesomeIcon icon={['fas', 'gear']} />,
+    selected: location.pathname === '/settings',
+    text: t('home.settings.menu'),
+    onClick: () => { navigate('/settings/general') }
+  }, {
+    icon: <FontAwesomeIcon icon={['fas', 'gifts']} />,
+    selected: location.pathname === '/credits',
+    text: t('home.credits.menu'),
+    onClick: () => { navigate('/credits') }
+  }]
+
+  return (
+    <Menu
+      title={t('home.title')}
+      items={items}
+    />
+  )
+
   return (
     <div className='ap-dom-general'>
       <h2>General</h2>
